e31e0bc9e6738200a32496becfb8a7d371e7c9b9
Ryan Riley <ryan.riley@panesofglass.org>
2010-11-11 17:19:13 -0800

    Updated reference to frack, added dependencies to Microsoft.Http, and updated Frank to use the State monad (renamed frank for this specific use). Started on helper methods for working with the monad, but more still to come.

--- src/Frank.fs
+++ src/Frank.fs
@@ -1,45 +1,113 @@
 ﻿namespace Frank
 open System
 open System.Collections.Generic
-open System.Text.RegularExpressions
+open System.Net
+open Microsoft.Http
 open Frack
 
-/// Defines available response types.
-type FrankResponse =
-  | Obj of obj
-  | Str of string
-  | Int of int
-  | Bool of bool
-  | ByteStr of bytestring
-  | Hash of IDictionary<string, obj>
-  | Response of Frack.Response
-  | Unauthorized
-  | Forbidden
-  | NotFound
-  | InternalServerError
+[<AutoOpen>]
+module Core =
+  open FSharp.Monad
 
-/// A route for a Frank applicaiton.
-type Route = { Pattern: Regex
-               Keys: seq<string>
-               Conditions: seq<IDictionary<string, string> -> bool>
-               Handler: IDictionary<string, string> -> FrankResponse }
+  /// Defines the standard Frank application type.
+  type App = Func<HttpRequestMessage, HttpResponseMessage>
+
+  /// Defines a handler for responding requests.
+  type FrankHandler = State<unit, HttpRequestMessage * IDictionary<string, string> * HttpResponseMessage>
+
+  let frank = State.StateBuilder()
+
+  /// Gets the current state of the request.
+  let getRequest = frank {
+    let! (req:HttpRequestMessage, _:IDictionary<string,string>, _:HttpResponseMessage) = getState
+    return req }
+
+  /// Gets the current state of the parameters.
+  let getParameters = frank {
+    let! (_:HttpRequestMessage, parms:IDictionary<string,string>, _:HttpResponseMessage) = getState
+    return parms }
+
+  /// Gets the current state of the response.
+  let getResponse = frank {
+    let! (_:HttpRequestMessage, _:IDictionary<string,string>, resp:HttpResponseMessage) = getState
+    return resp }
+
+  /// Updates the state of the request.
+  let putRequest req = frank {
+    let! (_:HttpRequestMessage, parms:IDictionary<string,string>, resp:HttpResponseMessage) = getState
+    do! putState (req, parms, resp) }
+
+  /// Updates the state of the response.
+  let putResponse resp = frank {
+    let! (req:HttpRequestMessage, parms:IDictionary<string,string>, _:HttpResponseMessage) = getState
+    do! putState (req, parms, resp) }
+
+  /// Updates the state of the response content.
+  let putContent content = frank {
+    let! (req:HttpRequestMessage, parms:IDictionary<string,string>, resp:HttpResponseMessage) = getState
+    resp.StatusCode <- HttpStatusCode.OK
+    resp.Content <- content
+    do! putState (req, parms, resp) }
+
+  /// Runs the FrankHandler 
+  let run (h: FrankHandler) initialState =
+    let (_, _, resp) = exec h initialState
+    resp
+
+  // TODO: Add more helper methods to mutate only portions of the request or response objects.
 
 [<AutoOpen>]
 module Routing =
+  open System.Text.RegularExpressions
+  open FSharp.Monad.Maybe
+  open Core
+
+  /// A route for a Frank application.
+  type Route = { Method     : string
+                 Path       : string
+                 Keys       : seq<string>
+                 Conditions : seq<HttpRequestMessage -> bool>
+                 Handler    : FrankHandler }
+
   /// Pulled from <see href="http://www.markhneedham.com/blog/2009/05/10/f-regular-expressionsactive-patterns/" />.
   let (|Match|_|) pattern input =
     let m = Regex.Match(input, pattern) in
-    if m.Success then Some(List.tail [ for g in m.Groups -> g.Value ]) else None
+    if m.Success then Some(List.tail [ for g in m.Groups -> g.Value ] |> List.toSeq) else None
 
+  /// Matches a target HTTP method or any method.
+  let matchMethod target = function m when m = "*" || m = target -> Some(target) | _ -> None 
+
+  /// Matches a url from the path specification.
+  let matchUrl path = function Match path result -> Some(result) | _ -> None
+
+  /// Matches a route and returns an option containing the params and handler. 
+  let (|Route|_|) (route:Route, request:HttpRequestMessage) = maybe {
+    // Ensure the method matches the request.
+    let! _ = matchMethod route.Method request.Method
+    // Match the request path and retrieve the parameters from the Uri match groups.
+    // The bind here is required to ensure the url path matches, as well as for retrieving the matching groups.
+    let! matches = matchUrl route.Path request.Uri.AbsolutePath
+    // Push the matches into a dictionary along with their keys.
+    let parms = Dictionary<string, string>() in Seq.zip route.Keys matches |> Seq.iter parms.Add
+    return (request, route.Handler, parms :> IDictionary<string, string>) }
+
+  /// Returns the path along with the expected match keys.
   let private compile path =
-    // Need to finish this method.
-    (Regex(path), Seq.empty<string>)
+    // Find all Regex match groups and add the group names as the keys.
+    let namedGroups = "\?\<([A-Za-z0-9_]+)\>"
+    let matches = Regex.Matches(path, namedGroups)
+    let keys = seq {
+      for m in matches do
+        // Don't forget to trim the initial match from the groups.
+        yield! List.tail [ for g in m.Groups -> g.Value ] |> List.toSeq }
+    (path, keys)
 
+  /// Creates a route object for a given HTTP method, path pattern, and Handler.
   let private route httpMethod path handler =
     let pattern, keys = compile path
-    let conditions = Seq.empty<IDictionary<string, string> -> bool>
-    (httpMethod, {Pattern = pattern; Keys = keys; Conditions = conditions; Handler = handler})
+    {Method = httpMethod; Path = pattern; Keys = keys; Conditions = Seq.empty; Handler = handler}
 
+  let map path handler = route "*" path handler
   let get path handler = route "GET" path handler
   let head path handler = route "HEAD" path handler
   let put path handler = route "PUT" path handler
@@ -47,54 +115,30 @@ module Routing =
   let delete path handler = route "DELETE" path handler
   let options path handler = route "OPTIONS" path handler
 
-/// Defines the standard Frank application type.
-type FrankApp(routes: seq<string * Route>) =
-  // Using a mutable dictionary here since we are only creating this once at the start of a FrankApp.
-  let router = routes
-               |> Seq.groupBy fst
-               |> Seq.map (fun (key, values) -> (key, values |> Seq.map snd))
-               |> dict
+[<AutoOpen>]
+module FrankApp =
+  open Core
+  open Routing
 
-  let parseResponse response =
-    // TODO: Would additional Active Patterns help here?
-    match response with
-    | Obj(v) -> let content = ByteString.fromString (v.ToString())
-                let contentLength = content |> Seq.length
-                (200, dict[("Content_Length", contentLength.ToString())], seq { yield content })
-    | Str(v) -> let content = ByteString.fromString v
-                let contentLength = content |> Seq.length
-                (200, dict[("Content_Length", contentLength.ToString())], seq { yield content })
-    | Int(v) -> let content = ByteString.fromString (v.ToString())
-                let contentLength = content |> Seq.length
-                (200, dict[("Content_Length", contentLength.ToString())], seq { yield content })
-    | Bool(v) -> let content = ByteString.fromString (v.ToString())
-                 let contentLength = content |> Seq.length
-                 (200, dict[("Content_Length", contentLength.ToString())], seq { yield content })
-    | ByteStr(v) -> let contentLength = v |> Seq.length
-                    (200, dict[("Content_Length", contentLength.ToString())], seq { yield v })
-    | Response(status, hdrs, body) -> (status, hdrs, body)
-    | Unauthorized -> (401, dict[("Content_Length", "12")], seq { yield ByteString.fromString "Unauthorized" })
-    | Forbidden -> (403, dict[("Content_Length", "9")], seq { yield ByteString.fromString "Forbidden" })
-    | InternalServerError -> (500, dict[("Content_Length", "21")], seq { yield ByteString.fromString "Internal Server Error" })
-    | _ -> (404, dict[("Content_Length", "9")], seq { yield ByteString.fromString "Not found" })
+  /// Initializes a Frank application with the specified sequence of routes.
+  let init (routes: seq<Route>) =
+    // TODO: Subscribe handlers to a request received event instead?
+    // TODO: If a given route returns a not found, can you continue, or fall through, to another route?
+    // Set a default not found response message.
+    let notFound = new HttpResponseMessage(StatusCode = HttpStatusCode.NotFound)
 
-  /// Finds the appropriate handler from the router and invokes it.
-  member this.Invoke(env:Environment) =
-    let request = Request(env)
-    let httpMethod = request.HttpMethod
-    let response =
-      if router.ContainsKey(httpMethod) then
-        let path = request.Uri.AbsolutePath
-        // TODO: Switch to the Match Active Pattern.
-        router.[httpMethod]
-        |> Seq.filter (fun r -> r.Pattern.IsMatch(path))
-        |> Seq.map (fun r -> 
-             let values = dict [| for c in r.Pattern.Match(path).Captures do
-                                    yield (path.Substring(c.Index, c.Length), c.Value) |]
-             try
-               r.Handler(values)
-             // TODO: Add additional error handling here, e.g. 401, 403, etc.
-             with _ -> InternalServerError)
-        |> Seq.head
-      else NotFound
-    parseResponse response
+    // Matches the route and request to a registered route.
+    let fromRoutes = function Route res -> Some(res) | _ -> None
+
+    // Executes the handler on the current state of the application.
+    let toResponse (request, handler, parms) = run handler (request, parms, notFound)
+
+    // Define the delegate that defines the Frank application. 
+    let app = Func<_,_>(fun request ->
+      // Find the first matching response. 
+      routes
+      |> Seq.map (fun route -> (route, request))
+      |> Seq.choose fromRoutes
+      |> Seq.map toResponse
+      |> Seq.head )
+    app
--- src/FrankSpecs.fs
+++ src/FrankSpecs.fs
@@ -2,55 +2,35 @@
 open System
 open System.Collections.Generic
 open System.Text
+open Microsoft.Http
 open Frack
 open Frank
 open NaturalSpec
 
-module Fakes =
-  open System.Collections.Specialized
-  open System.IO
-  open System.Web
-
-  let getRequest m (url:Uri) (errors:StringBuilder) =
-    seq { yield ("HTTP_METHOD", Frack.Str m)
-          yield ("SCRIPT_NAME", Frack.Str (url.AbsolutePath |> getPathParts |> fst))
-          yield ("PATH_INFO", Frack.Str (url.AbsolutePath |> getPathParts |> snd))
-          yield ("QUERY_STRING", Frack.Str (url.Query.TrimStart('?')))
-          yield ("CONTENT_TYPE", Frack.Str "text/plain")
-          yield ("CONTENT_LENGTH", Frack.Int 5)
-          yield ("SERVER_NAME", Frack.Str url.Host)
-          yield ("SERVER_PORT", Frack.Str (url.Port.ToString()))
-          yield! [|("HTTP_TEST", Frack.Str "value");("REQUEST_METHOD", Frack.Str m)|]
-          yield ("url_scheme", Frack.Str url.Scheme)
-          yield ("errors", Err ByteString.empty)
-          yield ("input", Inp (ByteString.fromString "Howdy"))
-          yield ("version", Ver [|0;1|] )
-        } |> dict
-
 //module RouteSpecs =
 
-module FrankAppSpecs =
-  open Fakes
+module HelloAppSpecs =
+  let helloHandler = frank { do! putContent (HttpContent.Create("Hello world!", "text/plain")) }
 
   [<Scenario>]
   let ``When creating a Frank applicaion, it should accept a sequence of routes``() =
-    let ``creating an app`` handlers =
+    let ``creating an app`` routes =
       printMethod ""
-      FrankApp handlers
-    Given [| get "/" (fun _ -> Str "Hello world!") |]
+      FrankApp.init routes
+    Given [ get "/" helloHandler ]
     |> When ``creating an app``
-    |> It should be (fun app -> app.GetType() = typeof<FrankApp>)
+    |> It should be (fun app -> app.GetType() = typeof<App>)
     |> Verify
 
   [<Scenario>]
   let ``When creating a Frank application, it should respond with Hello world!``() =
-    let helloworld = FrankApp [ get "/" (fun _ -> Str "Hello world!") ] 
-    let ``invoking an hello world app`` (app:FrankApp) =
+    let helloworld = FrankApp.init [ get "/" helloHandler ] 
+    let ``invoking an hello world app`` (app:App) =
       printMethod ""
       let url = Uri("http://wizardsofsmart.net/") 
-      let request = getRequest "GET" url (StringBuilder()) 
-      let status, hdrs, value = app.Invoke(request)
-      value |> Seq.head |> ByteString.toString
+      let request = new HttpRequestMessage("GET", url)
+      let response = app.Invoke(request)
+      response.Content.ReadAsString()
     Given helloworld
     |> When ``invoking an hello world app``
     |> It should equal "Hello world!" 
