ad7e2f9ab81c1263d06e1eb2bd817cb96097f0f4
Ryan Riley <ryan.riley@panesofglass.org>
2010-11-28 14:34:44 -0800

    Added spec for object serialization and conneg.

--- src/FrankAppSpecs.fs
+++ src/FrankAppSpecs.fs
@@ -1,17 +1,28 @@
-﻿module FrankSpecs
+﻿module Frank.AppSpecs
 open System
 open System.Collections.Generic
+open System.Runtime.Serialization
 open System.Text
 open Microsoft.Http
+open Frack
 open Frank
 open NaturalSpec
 
 module HelloAppSpecs =
-  let formatters = seq {
-    yield { ContentType = [| "application/xml";"text/xml" |]
-            Format = (fun (o,s,r) ->
-                        let f = new System.Xml.Serialization.XmlSerializer(o.GetType()) in f.Serialize(s, o)) }
-  }
+  [<DataContract>]
+  type TestType = {
+    [<field: DataMember(Name = "name")>]
+    Name:string;
+    [<field: DataMember(Name = "age")>]
+    Age:int }
+
+  let xmlFormatter = {
+    ContentType = [| "application/xml";"text/xml" |]
+    Format = (fun (o,s,r) -> let f = new DataContractSerializer(o.GetType()) in f.WriteObject(s, o)) }
+
+  let jsonFormatter = {
+    ContentType = [| "application/json";"text/json" |]
+    Format = (fun (o,s,r) -> let f = new Json.DataContractJsonSerializer(o.GetType()) in f.WriteObject(s, o)) }
 
   [<Scenario>]
   let ``When creating a Frank application, it should accept a sequence of routes``() =
@@ -24,15 +35,36 @@ module HelloAppSpecs =
     |> Verify
 
   [<Scenario>]
-  let ``When creating a Frank application, it should respond with Hello world!``() =
-    let helloworld = App [ get "/" (render "Hello world!") ] 
-    let ``invoking an hello world app`` (app:App) =
+  let ``When invoking a Frank application, it should respond with Hello world!``() =
+    let ``invoking an hello world app`` request =
       printMethod ""
-      let url = Uri("http://wizardsofsmart.net/") 
-      let request = new HttpRequestMessage("GET", url)
-      let response = app.Invoke(request)
+      let response = App([get "/" (render "Hello world!")]).Invoke(request)
       response.Content.ReadAsString()
-    Given helloworld
+    Given (new HttpRequestMessage("GET", Uri("http://wizardsofsmart.net/")))
     |> When ``invoking an hello world app``
     |> It should equal "Hello world!" 
+    |> Verify
+
+  [<Scenario>]
+  let ``When running a Frank application that renders an object, it should format the object.``() =
+    let request = new HttpRequestMessage("GET", Uri("http://wizardsofsmart.net/ryan"))
+    request.Headers.Add("Accept","application/xml")
+
+    let ryan = {Name = "Ryan"; Age = 31}
+
+    let expected =
+      let s = new DataContractSerializer(ryan.GetType())
+      let sb = new System.Text.StringBuilder()
+      use writer = System.Xml.XmlWriter.Create(sb)
+      s.WriteObject(writer, ryan)
+      sb.ToString()
+      
+    let ``invoking the application`` request =
+      printMethod ""
+      let response = App([get "/ryan" (render ryan)], formatters = [|xmlFormatter;jsonFormatter|]).Invoke(request)
+      response.Content.ReadAsByteString() |> ByteString.toString
+
+    Given request
+    |> When ``invoking the application``
+    |> It should equal expected 
     |> Verify
