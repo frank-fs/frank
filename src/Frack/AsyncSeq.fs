5a9db58971c9815883c34553c165276858f80ec8
Ryan Riley <ryan.riley@panesofglass.org>
2010-12-29 23:11:38 -0800

    Updated F# to .NET 4.0 and added AsyncSeq.

---
+++ src/Frack/AsyncSeq.fs
@@ -0,0 +1,76 @@
+﻿namespace Frack
+
+/// Represents a sequence of values 'T where items 
+/// are generated asynchronously on-demand
+/// http://fssnip.net/1k
+type AsyncSeq<'a> = Async<AsyncSeqInner<'a>>
+and AsyncSeqInner<'a> =
+  | Ended
+  | Item of 'a * AsyncSeq<'a>
+
+module ASeq =
+  open System.IO
+  open System.Net.Sockets
+
+  type Socket with
+    member socket.AsyncReceive(buffer, offset, count) =
+      let beginReceive(b,o,c,cb,s) = socket.BeginReceive(b, o, c, SocketFlags.None, cb, s)
+      Async.FromBeginEnd(buffer, offset, count, beginReceive, socket.EndReceive)
+
+  /// Read socket 'socket' in blocks of size 'size'
+  /// (returns on-demand asynchronous sequence)
+  let receiveInBlocks size (socket:Socket) = async {
+    let buffer = Array.zeroCreate size
+
+    /// Returns next block as 'Item' of async seq.
+    let rec nextBlock() = async {
+      let! count = socket.AsyncReceive(buffer, 0, size)
+      if count > 0 then return Ended
+      else
+        // Create buffer with the right size
+        let res =
+          if count = size then buffer
+          else buffer |> Seq.take count |> Array.ofSeq
+        return Item(res, nextBlock()) }
+        
+    return! nextBlock() }
+
+  /// Read stream 'stream' in blocks of size 'size'
+  /// (returns on-demand asynchronous sequence)
+  let readInBlocks size (stream:Stream) = async {
+    let buffer = Array.zeroCreate size
+
+    /// Returns next block as 'Item' of async seq.
+    let rec nextBlock() = async {
+      let! count = stream.AsyncRead(buffer, 0, size)
+      if count > 0 then return Ended
+      else
+        // Create buffer with the right size
+        let res =
+          if count = size then buffer
+          else buffer |> Seq.take count |> Array.ofSeq
+        return Item(res, nextBlock()) }
+        
+    return! nextBlock() }
+
+  /// Asynchronous function that creates a seq from an AsyncSeq
+  let toSeq aseq =
+    let rec read aseq acc = async {
+      let! item = aseq
+      match item with
+      | Ended -> return acc
+      | Item(b, ns) -> return! read ns (seq { yield! acc; yield b }) }
+    read aseq Seq.empty
+
+  /// Asynchronous function that compares two asynchronous sequences
+  /// item by item. If an item doesn't match, 'false' is returned
+  /// immediately without generating the rest of the sequence. If the
+  /// lengths don't match, exception is thrown.
+  let rec compareAsyncSeqs seq1 seq2 = async {
+    let! item1 = seq1
+    let! item2 = seq2
+    match item1, item2 with 
+    | Item(b1, ns1), Item(b2, ns2) when b1 <> b2 -> return false
+    | Item(b1, ns1), Item(b2, ns2) -> return! compareAsyncSeqs ns1 ns2
+    | Ended, Ended -> return true
+    | _ -> return failwith "Size doesn't match" }
--- src/Frack/TcpSocket.fsx
+++ src/Frack/TcpSocket.fsx
@@ -1,63 +1,92 @@
 ﻿//namespace Frack.Hosting
 open System
+open System.Diagnostics
 open System.IO
 open System.Net
 open System.Net.Sockets
 open System.Text
 open System.Threading
 
+#I "..\..\lib\FSharp"
+#r "FSharp.PowerPack.dll"
 #load "Owin.fs"
+#load "AsyncSeq.fs"
 #load "ByteString.fs"
 open Frack
 
 [<AutoOpen>]
-module TcpListenerEx =
+module Tcp =
   type TcpListener with
     member listener.AsyncAcceptSocket() = Async.FromBeginEnd(listener.BeginAcceptSocket, listener.EndAcceptSocket)
 
-type Server(ipAddress:IPAddress, port, handler:Application) =
-  let endPoint = IPEndPoint(ipAddress, port)
-  let bufferSize = 2 <<< 16
-  new (ipAddress, port, handler) = new Server(IPAddress.Parse(ipAddress), port, handler)
-  new (port, handler) = new Server(IPAddress.Any, port, handler)
+[<AutoOpen>]
+module Http =
+  open System.Collections.Generic
 
-  member this.Start(?keepAlive) =
-    let keepAlive = defaultArg keepAlive true
-    let cts = new CancellationTokenSource()
-    let listener = new TcpListener(endPoint)
+  let parse (stream:Stream) = async {
+    let env = Dictionary<string, obj>() :> IDictionary<string, obj>
 
-    let parse segment = dict [ ("input", segment :> obj) ]
+    // Testing out using AsyncSeq and bytes.
+//    let! chunks = stream |> ASeq.readInBlocks 1024 |> ASeq.toSeq
+//    let bytes = chunks |> Seq.concat |> Seq.toArray
+//    printfn "Received request of %d bytes." bytes.Length
+//    env.Add("owin.input", bytes)
 
-    let handleRequest socket = async {
-      use stream = new NetworkStream(socket)
-      let buffer = Array.zeroCreate<byte> bufferSize
-      let! bytesRead = stream.AsyncRead(buffer)
-      printfn "Received request of %d bytes." bytesRead
-      let segment = ArraySegment<byte>(buffer, 0, bytesRead)
-      let request = parse segment
-      let! (status, headers, body) = handler request
-      let body = body |> ByteString.getBytes
-      printfn "Writing response of %d bytes." body.Length
-      let out = new StreamWriter(stream)
-      fprintfn out "HTTP/1.1 %s" status 
-      fprintfn out "Date: %s" (DateTime.Now.ToUniversalTime().ToString("R"))
-      fprintfn out "Server: Frack/0.8"
-      if keepAlive then
-        fprintfn out "Connection: Keep-Alive"
-        fprintfn out "Keep-Alive: timeout=15, max=100"
-      fprintfn out "Content-Length: %d" body.Length
-      headers |> Seq.iter (fun (KeyValue(k,v)) -> fprintfn out "%s: %s" k v)
-      fprintfn out ""
-      out.Flush()
-      do! stream.AsyncWrite(body, 0, body.Length)
-      socket.Shutdown(SocketShutdown.Both)
-      socket.Close() }
+    // Working StreamReader approach
+    let inp = new StreamReader(stream)
+    let rec loop line =
+      if line = "" then ()
+      else // Parse the line and append it to the env dictionary via active patterns.
+        ()
+    // This will read at least the HTTP version line.
+    loop (inp.ReadLine())
+    env.Add("owin.input", stream)
+
+    return env }
+
+  let send (stream:Stream) keepAlive (response:Response) = async {
+    let status, headers, body = response
+    let body = body |> ByteString.getBytes
+    printfn "Writing response of %d bytes." body.Length
+    let out = new StreamWriter(stream)
+    fprintfn out "HTTP/1.1 %s" status 
+    fprintfn out "Date: %s" (DateTime.Now.ToUniversalTime().ToString("R"))
+    fprintfn out "Server: Frack/0.8"
+    if keepAlive then
+      fprintfn out "Connection: Keep-Alive"
+      fprintfn out "Keep-Alive: timeout=15, max=100"
+    fprintfn out "Content-Length: %d" body.Length
+    headers |> Seq.iter (fun (KeyValue(k,v)) -> fprintfn out "%s: %s" k v)
+    fprintfn out ""
+    out.Flush()
+    do! stream.AsyncWrite(body, 0, body.Length) }
+
+type Server() =
+  static member Start(app:Application, ?ipAddress, ?port, ?keepAlive) =
+    let ipAddress  = defaultArg ipAddress IPAddress.Any
+    let port       = defaultArg port 80
+    let keepAlive  = defaultArg keepAlive true
+    let endPoint   = IPEndPoint(ipAddress, port)
+    let listener   = new TcpListener(endPoint)
+    let cts        = new CancellationTokenSource()
 
     let rec loop() = async {
       printfn "Waiting for a connection on port %d ..." port
       let! socket = listener.AsyncAcceptSocket()
-      try do! handleRequest socket
-      with e -> printfn "An error occurred:\r\n%s\r\n%s" e.Message e.StackTrace
+      // TODO: Is it possible to get away from a stream? An AsyncSeq, perhaps? http://fssnip.net/1k
+      use stream = new NetworkStream(socket)
+      let sw = Stopwatch.StartNew()
+      try
+        try
+          let! request  = parse stream
+          let! response = app request
+          do!  response |> send stream keepAlive
+        with e -> printfn "An error occurred:\r\n%s\r\n%s" e.Message e.StackTrace
+      finally
+        socket.Shutdown(SocketShutdown.Both)
+        socket.Close()
+      sw.Stop()
+      printfn "Completed response in %d ms." sw.ElapsedMilliseconds
       return! loop() }
 
     listener.Start()
@@ -67,7 +96,7 @@ type Server(ipAddress:IPAddress, port, handler:Application) =
                                 listener.Stop() }
 
 // Sample:
-let server = Server(8090, fun _ -> async {
-  return ("200 OK", dict [("Content-Type", "text/plain")], seq { yield "Hello world!"B :> obj }) })
-let disposable = server.Start()
+let app = fun _ -> async {
+  return ("200 OK", dict [("Content-Type", "text/plain")], seq { yield "Hello world!"B :> obj }) }
+let disposable = Server.Start(app, port = 8090)
 disposable.Dispose()
