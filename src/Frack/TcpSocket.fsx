78495694689c7c951cfeebbd967014917e26e017
Ryan Riley <ryan.riley@panesofglass.org>
2011-01-06 22:47:50 -0800

    Updated TcpSocket.fsx sample to echo the incoming request.

--- src/Frack/TcpSocket.fsx
+++ src/Frack/TcpSocket.fsx
@@ -18,8 +18,12 @@ open System.Threading
 open Frack
 
 // Sample:
-let app = fun _ -> async {
-  return ("200 OK", dict [("Content-Type", "text/plain")], seq { yield "Hello world!"B :> obj }) }
+let app = fun (req:Request) -> async {
+  let folder (builder:StringBuilder) (KeyValue(k,v)) =
+    builder.AppendLine(sprintf "%s: %s" k (v.ToString()))
+  let body = Seq.fold folder (StringBuilder()) req
+  let bytes = body.ToString() |> Http.ascii
+  return ("200 OK", dict [("Content-Type", "text/plain")], seq { yield bytes :> obj }) }
 let disposable = Server.Start(app, port = 8090)
 Thread.Sleep(60 * 1000)
 printfn "Closing ..."
