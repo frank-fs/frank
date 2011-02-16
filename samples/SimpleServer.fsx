b597f77aa2c027f65041eeda64ecf28a8d0cb860
Ryan Riley <ryan.riley@panesofglass.org>
2011-02-15 22:38:52 -0800

    Updated SimpleServer.fsx HttpListener sample.

--- samples/SimpleServer.fsx
+++ samples/SimpleServer.fsx
@@ -1,5 +1,4 @@
 ﻿// SimpleServer is an example of using Frack as a F# script.
-// <see href="http://blogs.msdn.com/b/chrsmith/archive/2008/09/12/scripting-in-f.aspx" />
 #I @"..\lib\FSharp"
 #I @"..\src\Frack\bin\Debug"
 #I @"..\src\Frack.HttpListener\bin\Debug"
@@ -7,7 +6,7 @@
 #r "System.Net.dll"
 #r "System.Web.dll"
 #r "System.Web.Abstractions.dll"
-#r "owin.dll"
+#r "Frack.Collections.dll"
 #r "frack.dll"
 #r "Frack.HttpListener.dll"
 
@@ -17,6 +16,7 @@ open System.IO
 open System.Net
 open System.Threading
 open Frack
+open Frack.Collections
 open Frack.Middleware
 open Frack.Hosting.HttpListener
 
@@ -25,22 +25,13 @@ printfn "Creating server ..."
 let cts = new CancellationTokenSource()
 
 // Define the application function.
-let appAsync = fun request -> async { 
-  return Response.Create("200 OK",
-                         (dict [("Content_Type", seq { yield "text/plain" });("Content_Length", seq { yield "6" })]),
-                         "Howdy!") }
-
-// Define the application.
-let app = Application(appAsync)
+let app = Owin.FromAsync(fun request -> async { 
+  return "200 OK", dict [("Content_Type", "text/plain" )], seq { yield "Howdy!"B :> obj } })
 
 // Set up and start an HttpListener
 HttpListener.Start("http://localhost:9191/", app, cts.Token)
 printfn "Listening on http://localhost:9191/ ..."
 
-let stopListening() =
-  app.Stop()
-  cts.Cancel()
-  printfn "Stopped listening."
-
-System.Console.ReadKey() |> ignore
-stopListening()
+Thread.Sleep(60 * 1000)
+cts.Cancel()
+printfn "Stopped listening."
