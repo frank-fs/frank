dd0f4a8d99ff26eda266e3c52854c2496ac19ba8
Ryan Riley <ryan.riley@panesofglass.com>
2011-11-27 15:37:32 -0800

    Add an ASP.NET integration sample that shows off routing and middleware.

---
+++ samples/AspNetSamples/AspNetIntegration/App/Global.fs
@@ -0,0 +1,29 @@
+﻿namespace FSharpMVC3.Core
+
+open System
+open System.Net
+open System.Net.Http
+open System.Web.Routing
+open Frank
+open Frank.Hosting
+
+type Global() =
+    inherit System.Web.HttpApplication() 
+
+    // Respond with a plain text "Hello, world!"
+    let helloWorld _ _ = respond HttpStatusCode.OK (``Content-Type`` "text/plain") <| Some("Hello, world!")
+
+    // Respond with the request content, if any.
+    let echo _ (content: HttpContent) =
+      respond HttpStatusCode.OK (``Content-Type`` "text/plain") <| Some(content.ReadAsString())
+    
+    let resource = route "/" (get helloWorld <|> post echo)
+
+    // Mount the app and add a middleware to support HEAD requests.
+    let app : HttpApplication = mountWithDefaults [ resource ] |> Middleware.head
+    
+    let config = WebApi.configure app
+    let baseUri = "http://localhost:1000/"
+
+    member x.Start() =
+        RouteTable.Routes.MapServiceRoute<WebApi.FrankApi>("", config)
---
+++ samples/AspNetSamples/AspNetIntegration/Web/Global.asax
@@ -0,0 +1,9 @@
+﻿<%@ Application Inherits="FSharpMVC3.Core.Global" Language="C#" %>
+<script Language="C#" RunAt="server">
+
+  protected void Application_Start(Object sender, EventArgs e) {
+    // Delegate event handling to the F# Application class
+    base.Start();
+  }
+
+</script>
