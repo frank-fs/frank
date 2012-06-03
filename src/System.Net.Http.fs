6648b8bb12646d63179ec33d5258f75549f8f01c
Ryan Riley <ryan.riley@panesofglass.org>
2012-06-03 13:02:46 -0600

    Add missing async methods and other minor extension cleanup.

--- src/System.Net.Http.fs
+++ src/System.Net.Http.fs
@@ -13,11 +13,14 @@ namespace System.Net.Http
 open System.Net.Http
 open System.Net.Http.Formatting
 open System.Net.Http.Headers
+open System.Threading.Tasks
 
 type EmptyContent() =
   inherit HttpContent()
   override x.SerializeToStreamAsync(stream, context) =
-    System.Threading.Tasks.Task.Factory.StartNew(fun () -> ())
+    let tcs = new TaskCompletionSource<_>(TaskCreationOptions.None)
+    tcs.SetResult(())
+    tcs.Task :> Task
   override x.TryComputeLength(length) =
     length <- 0L
     true
@@ -39,13 +42,17 @@ module Extensions =
   open System.Net.Http
   open System.Net.Http.Headers
 
+  let private emptyContent = new EmptyContent() :> HttpContent
+
   type HttpContent with
-    static member Empty = new EmptyContent() :> HttpContent
+    static member Empty = emptyContent
     member x.AsyncReadAs<'a>() = Async.AwaitTask <| x.ReadAsAsync<'a>()
     member x.AsyncReadAs<'a>(formatters) = Async.AwaitTask <| x.ReadAsAsync<'a>(formatters)
     member x.AsyncReadAs(type') = Async.AwaitTask <| x.ReadAsAsync(type')
     member x.AsyncReadAs(type', formatters) = Async.AwaitTask <| x.ReadAsAsync(type', formatters)
     member x.AsyncReadAsByteArray() = Async.AwaitTask <| x.ReadAsByteArrayAsync()
+    member x.AsyncReadAsHttpRequestMessage() = Async.AwaitTask <| x.ReadAsHttpRequestMessageAsync()
+    member x.AsyncReadAsHttpResponseMessage() = Async.AwaitTask <| x.ReadAsHttpResponseMessageAsync()
     member x.AsyncReadAsMultipart() = Async.AwaitTask <| x.ReadAsMultipartAsync()
     member x.AsyncReadAsMultipart(streamProvider) = Async.AwaitTask <| x.ReadAsMultipartAsync(streamProvider)
     member x.AsyncReadAsMultipart(streamProvider, bufferSize) = Async.AwaitTask <| x.ReadAsMultipartAsync(streamProvider, bufferSize)
