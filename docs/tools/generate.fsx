998c98bd68bd83436219a49f9fa6e3576fd41dc9
Ryan Riley <ryan.riley@panesofglass.org>
2014-01-07 09:58:03 -0600

    Fix generated documentation path.

--- docs/tools/generate.fsx
+++ docs/tools/generate.fsx
@@ -6,7 +6,7 @@
 // Binaries that have XML documentation (in a corresponding generated XML file)
 let referenceBinaries = [ "Frank.dll" ]
 // Web site location for the generated documentation
-let website = "/Frank"
+let website = "/frank"
 
 // Specify more information about your project
 let info =
