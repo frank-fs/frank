6c870efc20dd577591099e75256a4fd8a222e3a9
panesofglass <ryan.riley@panesofglass.org>
2014-01-08 00:40:31 -0600

    Add runConneg sample to WebHost project.

--- samples/WebHost/Global.asax.fs
+++ samples/WebHost/Global.asax.fs
@@ -7,6 +7,7 @@ open System.Web
 open System.Web.Http
 open System.Web.Http.HttpResource
 open Newtonsoft.Json
+open Frank
 
 [<CLIMutable>]
 [<JsonObject(MemberSerialization=MemberSerialization.OptOut)>]
@@ -21,10 +22,18 @@ type Global() =
     member x.Application_Start() =
         let values = [| { Make = "Ford"; Model = "Mustang" }; { Make = "Nissan"; Model = "Titan" } |]
 
-        let carsHandler (request: HttpRequestMessage) =
-            request.CreateResponse(HttpStatusCode.OK, values)
-            |> async.Return
+        // Use the HttpRequestMessage extension
+//        let carsHandler (request: HttpRequestMessage) =
+//            request.CreateResponse(HttpStatusCode.OK, values)
+//            |> async.Return
+
+        // Use the Frank runConneg helper to generate the response
+        let carsHandler =
+            runConneg GlobalConfiguration.Configuration.Formatters
+            <| fun _ -> async.Return values
             
+        // Create a resource using the HttpResource.route function
         let carsResource = route "/api/cars" <| get carsHandler
 
+        // Register HTTP resources using the HttpResource.register function
         GlobalConfiguration.Configure(fun config -> register [carsResource] config)
