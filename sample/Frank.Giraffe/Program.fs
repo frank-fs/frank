89b95a14a8f4b1d38f7527f86aed350e3cab3699
Ryan Riley <ryanriley@live.com>
2020-11-18 08:32:32 -0600

    Update to .NET 5.0

--- sample/Frank.Giraffe/Program.fs
+++ sample/Frank.Giraffe/Program.fs
@@ -15,46 +15,41 @@ open Frank.Giraffe.Extensions
 // Models
 // ---------------------------------
 
-type Message =
-    {
-        Text : string
-    }
+type Message = { Text: string }
 
 // ---------------------------------
 // Views
 // ---------------------------------
 
 module Views =
-    open GiraffeViewEngine
+    open Giraffe.ViewEngine
 
     let layout (content: XmlNode list) =
         html [] [
             head [] [
-                title []  [ encodedText "Frank.Giraffe" ]
-                link [ _rel  "stylesheet"
+                title [] [ encodedText "Frank.Giraffe" ]
+                link [ _rel "stylesheet"
                        _type "text/css"
                        _href "/main.css" ]
             ]
             body [] content
         ]
 
-    let partial () =
-        h1 [] [ encodedText "Frank.Giraffe" ]
+    let partial () = h1 [] [ encodedText "Frank.Giraffe" ]
 
-    let index (model : Message) =
-        [
-            partial()
-            p [] [ encodedText model.Text ]
-        ] |> layout
+    let index (model: Message) =
+        [ partial ()
+          p [] [ encodedText model.Text ] ]
+        |> layout
 
 // ---------------------------------
 // Web app
 // ---------------------------------
 
-let indexHandler (name : string) =
+let indexHandler (name: string) =
     let greetings = sprintf "Hello %s, from Giraffe!" name
-    let model     = { Text = greetings }
-    let view      = Views.index model
+    let model = { Text = greetings }
+    let view = Views.index model
     htmlView view
 
 let helloWorld =
@@ -67,44 +62,38 @@ let helloName =
     resource "/hello/{name}" {
         name "Hello Name"
         get (fun next ctx ->
-            let name = ctx.GetRouteValue("name") |> string
-            indexHandler name next ctx)
+                let name = ctx.GetRouteValue("name") |> string
+                indexHandler name next ctx)
     }
 
 // ---------------------------------
 // Error handler
 // ---------------------------------
 
-let errorHandler (ex : Exception) (logger : ILogger) =
+let errorHandler (ex: Exception) (logger: ILogger) =
     logger.LogError(ex, "An unhandled exception has occurred while executing the request.")
-    clearResponse >=> setStatusCode 500 >=> text ex.Message
+    clearResponse
+    >=> setStatusCode 500
+    >=> text ex.Message
 
 // ---------------------------------
 // Config and Main
 // ---------------------------------
 
-let configureCors (builder:CorsPolicyBuilder) =
-    builder.WithOrigins("http://localhost:8080")
-           .AllowAnyMethod()
-           .AllowAnyHeader()
-           |> ignore
+let configureCors (builder: CorsPolicyBuilder) =
+    builder.WithOrigins("http://localhost:8080").AllowAnyMethod().AllowAnyHeader()
+    |> ignore
 
-let configureLogging (builder:ILoggingBuilder) =
-    builder.AddFilter(fun l -> l.Equals LogLevel.Error)
-           .AddConsole()
-           .AddDebug()
+let configureLogging (builder: ILoggingBuilder) =
+    builder.AddFilter(fun l -> l.Equals LogLevel.Error).AddConsole().AddDebug()
 
 [<EntryPoint>]
 let main args =
     let contentRoot = Directory.GetCurrentDirectory()
-    let webRoot     = Path.Combine(contentRoot, "WebRoot")
+    let webRoot = Path.Combine(contentRoot, "WebRoot")
 
     webHost args {
-        configure (fun bldr ->
-            bldr.UseKestrel()
-                .UseContentRoot(contentRoot)
-                .UseIISIntegration()
-                .UseWebRoot(webRoot))
+        configure (fun bldr -> bldr.UseKestrel().UseContentRoot(contentRoot).UseIISIntegration().UseWebRoot(webRoot))
         logging configureLogging
         useCors configureCors
         // enable Giraffe content negotiation
